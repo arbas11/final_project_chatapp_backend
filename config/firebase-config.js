51de0d2a95f6e1a43c8faf8a40338afd42790f85
ario baskoro <ariobaskoro@arios-MacBook-Pro-2.local>
2022-04-05 21:14:25 +0700

    service account

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,6 +1,7 @@
 const admin = require("firebase-admin");
+require("dotenv").config();
 
-const serviceAccount = require("./serviceAccount.json");
+const serviceAccount = JSON.parse(process.env.GOOGLE_CREDS);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
