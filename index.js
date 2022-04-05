f3bcd915411310605e6f2a07bf961fa3958b3f15
ario baskoro <ariobaskoro@arios-MacBook-Pro-2.local>
2022-04-05 22:31:58 +0700

    change origin url

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(isAuth);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://stormy-tundra-60287.herokuapp.com/",
     methods: ["GET", "POST"],
   },
 });
